43d6bfb79b2353f65ac835c2d837d657807592ee
luisa Peña Guerrero <luisafdapg107@gmail.com>
2026-05-27 11:10:36 -0500

    feat(Ejercicio 2): Se creó la clase persona con atributos que heredó estdiante, se agregó a el main existente la prueba de este para comprobar su funcionamiento

---
+++ src/taller8/Estudiante.java
@@ -0,0 +1,15 @@
+package taller8;
+
+public class Estudiante  extends Persona{
+    private String matricula;
+
+    public Estudiante(String nombre, int edad, String matricula){
+        super(nombre, edad);
+        this.matricula = matricula;
+    }
+    @Override
+    public void mostrarDetalles(){
+        super.mostrarDetalles();
+        System.out.println("Matrícula = " + matricula);
+    }
+}
--- src/taller8/mainprueba.java
+++ src/taller8/mainprueba.java
@@ -4,6 +4,12 @@ public class mainprueba {
     public static void main(String[] args) {
         Coche coche = new Coche("Mazda", 150, 4);
         coche.mostrarInformacion();
-
     }
 }
+class claseprueba {
+    public static void main(String[] args) {
+        Estudiante estudiante = new Estudiante("Jose Perez", 26, "YT-2006-07");
+        estudiante.mostrarDetalles();
+    }
+}
+
